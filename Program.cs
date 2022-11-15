33423d63d61b794fb540f9a3b669269f9f50e497
zmacconnell <zachary.macconnell@gmail.com>
2022-11-15 11:16:50 -0700

    Added second cycle

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace Unit05
             Cast cast = new Cast();
             // cast.AddActor("food", new Food());
             cast.AddActor("cycle", new Cycle());
+            cast.AddActor("cycle2", new Cycle());
             cast.AddActor("score", new Score());
 
             // create the services
